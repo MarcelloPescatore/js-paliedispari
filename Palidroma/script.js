d3c90d80de5282b1d95618fbd0c2a425ef374119
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-04 15:23:21 +0200

    initial commit first assignment

---
+++ Palidroma/script.js
@@ -0,0 +1,13 @@
+// console.log('test');
+
+// Palidroma
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+// tools :
+// - variabili
+// - function
+// - condition
+
+
+
+
